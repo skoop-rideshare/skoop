74f75757b352d5c2bff657a1e930a8c7485b7f4a
Anton Björn Ivarsson <anton.ivarsson@sellpy.se>
2019-08-21 16:44:36 +0200

    Add login thats kind of broken.

--- src/login/actions.js
+++ src/login/actions.js
@@ -1,6 +1,18 @@
+import { handleLoginUser } from '../APIHandler.js'
 export const LOGIN_USER_LOADING = 'LOGIN_USER_LOADING'
-export const loginUser = () => {
-  return (dispatch) =>  {
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
+export const LOGIN_USER_ERROR = 'LOGIN_USER_ERROR'
+
+export const loginUser = (username,password) => {
+  return (dispatch) => {
     dispatch({type: LOGIN_USER_LOADING})
+    console.log('hej')
+    handleLoginUser(username, password)
+      .then(({ data: { user } }) => {
+        dispatch({type: LOGIN_USER_SUCCESS, user})
+      })
+      .catch(() => {
+        dispatch({ type: LOGIN_USER_ERROR })
+      })
   }
 }
--- src/login/reducer.js
+++ src/login/reducer.js
@@ -1,12 +1,26 @@
 import * as actions from './actions'
+import { combineReducers } from 'redux'
 
 const loading = (state = false, action) => {
   switch(action.type) {
     case actions.LOGIN_USER_LOADING:
       return true
+    case actions.LOGIN_USER_SUCCESS:
     default:
       return false
   }
 }
 
-export default loading
+const user = (state = {}, action) => {
+  switch(action.type) {
+  case actions.LOGIN_USER_SUCCESS:
+      return action.payload
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  loading,
+  user
+})
